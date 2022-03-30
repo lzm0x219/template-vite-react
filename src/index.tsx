cb7f3a969f84613da27d42ade3bf269b1c2f7387
うまる <lzm0x219@gmail.com>
2022-03-30 08:02:31 +0800

    chore(deps): update react monorepo to v18 (major)

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 
-render(
+const container = document.getElementById("root") as HTMLElement;
+
+const root = createRoot(container);
+
+root.render(
   <StrictMode>
     <App />
-  </StrictMode>,
-  document.getElementById("root")
+  </StrictMode>
 );
