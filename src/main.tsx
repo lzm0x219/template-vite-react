b625eab31afee5b914a399eef9e484aa80d9706f
0x219 <lzm0x219@gmail.com>
2022-09-09 21:15:04 +0800

    Update routes config

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,21 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import routes from "@/routes";
-import "./global";
+import App from "@/App";
+import "@/global";
 
 const container = document.getElementById("root") as HTMLElement;
 
 const root = createRoot(container);
 
-const router = createBrowserRouter(routes);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: routes,
+    errorElement: <div>error</div>,
+  },
+]);
 
 root.render(
   <StrictMode>
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,34 +1,25 @@
 import { lazy, Suspense } from "react";
 import { type RouteObject } from "react-router-dom";
-import App from "@/App";
 
 const Index = lazy(() => import("@/routes/index"));
 const Notfound = lazy(() => import("@/routes/404"));
 
 export const routes: Array<RouteObject> = [
   {
-    path: "/",
-    element: <App />,
-    // TODO: errorElement
-    errorElement: <div>error</div>,
-    children: [
-      {
-        index: true,
-        element: (
-          <Suspense>
-            <Index />
-          </Suspense>
-        ),
-      },
-      {
-        path: "*",
-        element: (
-          <Suspense>
-            <Notfound />
-          </Suspense>
-        ),
-      },
-    ],
+    index: true,
+    element: (
+      <Suspense>
+        <Index />
+      </Suspense>
+    ),
+  },
+  {
+    path: "*",
+    element: (
+      <Suspense>
+        <Notfound />
+      </Suspense>
+    ),
   },
 ];
 
