d3059e81693416e0e0807b1db8b614c525814383
0x219 <lzm0x219@gmail.com>
2022-10-26 10:15:05 +0800

    Remove requiring-type-checking

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,6 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
